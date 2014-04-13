9d0efde299680ef64e9cd6be821e6537c22c683b
fazhar <fazhar727@gmail.com>
2014-04-13 10:12:43 -0700

    First commit

---
+++ network.scm
@@ -0,0 +1,14 @@
+;An implementation of the game Network in Scheme.
+;
+;Integers used to describe players:
+;
+;   0 - Player with black pieces
+;   1 - Player with white pieces
+;   -1 - No player
+
+;Getting valid positions and manipulating the board.
+
+
+;Finding Networks and determining if a player has won.
+
+(define (has_won player) ) 
