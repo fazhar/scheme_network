631c8117ca46c1370a541b7cdaa693aa1fe8a7d9
Daguan Lu <daguanlu@gmail.com>
2014-04-16 16:24:29 -0700

    update utils

--- network.scm
+++ network.scm
@@ -11,24 +11,30 @@
 
 ;Utility functions
 
-(define (insert-sorted lst item comp-less-than)
+(define (insert-sorted item lst comp)
     (if (null? lst) (list item)
-        (if (comp-less-than item (car lst)) 
+        (if (> 0 (comp (car lst))) 
             (cons item lst)
-            (cons (car lst) (insert-sorted (cdr lst) item comp-less-than)))))
-            
+            (cons (car lst) (insert-sorted (cdr lst) item comp)))))
 
 (define (contains? lst item comp)
     (if (null? lst)
         #f
-        (or (comp item (car lst)) (contains? (cdr lst) item comp))))
+        (or (= 0 (comp item (car lst))) (contains? (cdr lst) item comp))))
     
+;NOTE, this function is inclusive
+(define (in-range? x range comp)
+    (and (<= (comp (car range) x) 0)(>= (comp (cadr range) x) 0)))
+
 
 ;Position is a list where the first item is the x-coordinate, and the second is the y coordinate
 (define (position x y)
     (list x y))
-(define (position=? p1 p2) 
-    (equal? p1 p2))
+(define (comp-positions p1 p2) 
+    (cond
+        ((equal? p1 p2) 0)
+        ((or (< (y-coor p1)(y-coor p2))(and (= (y-coor p1)(y-coor p2))(< (x-coor p1)(x-coor p2)))) -1)
+        (else 1)))
 (define (x-coor p) (car p))
 (define (y-coor p) (cadr p))
 (define (delta-x p1 p2)
@@ -51,7 +57,7 @@
 (define (is-valid? position color game)
     (let ((blacks (black-pieces game))(whites (white-pieces game)))
         (and
-            (not (or (contains? blacks position position=?) (contains? whites position position=?))) 
+            (not (or (contains? blacks position comp-positions) (contains? whites position comp-positions))) 
             (if (= color BLACK) 
                 (is-valid-helper? blacks position #f)
                 (is-valid-helper? whites position #f)))))
@@ -61,14 +67,14 @@
     (or (null? mypieces)
         (let ((cur (car mypieces)) (cur-neighbor? (neighbors? position (car mypieces))))
         (and (not (and has-neighbor cur-neighbor?))
-            (is-valid-helper? (cdr pieces) position (or has-neighbor cur-neighbor?))))))
+            (is-valid-helper? (cdr mypieces) position (or has-neighbor cur-neighbor?))))))
 
 ;Return a new game board if the move is valid. Otherwise, return nil.
 (define (make-add-move current-game color position)
     (if (is-valid? position color current-game)
         (if (= BLACK color)
-            (game (sorted-insert (black-pieces current-game) position) (white-pieces current-game))
-            (game (black-pieces current-game) (insert-sorted (white-pieces current-game) position)))))
+            (game (sorted-insert position (black-pieces current-game)) (white-pieces current-game))
+            (game (black-pieces current-game) (insert-sorted position (white-pieces current-game))))))
 
     
 ;Finding Networks and determining if a player has won.
